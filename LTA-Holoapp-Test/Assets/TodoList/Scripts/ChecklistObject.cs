089360da104506803c33f8f42bbb77a350f90045
pravindkk <34647779+pravindkk@users.noreply.github.com>
2022-05-21 23:58:30 +0800

    finished prefabs for todo

---
+++ LTA-Holoapp-Test/Assets/TodoList/Scripts/ChecklistObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.OpenXR.BasicSample
+{
+    public class ChecklistObject : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
