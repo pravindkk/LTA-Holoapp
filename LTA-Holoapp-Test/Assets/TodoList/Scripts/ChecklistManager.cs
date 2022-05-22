1eb5753062b80ae3017ed323ef14b76f0ff19760
pravindkk <34647779+pravindkk@users.noreply.github.com>
2022-05-22 10:06:57 +0800

    trying to connect todo script to scene

---
+++ LTA-Holoapp-Test/Assets/TodoList/Scripts/ChecklistManager.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+namespace Microsoft.MixedReality.OpenXR.BasicSample
+{
+    public class ChecklistManager : MonoBehaviour
+    {
+        public Transform content;
+        public GameObject addPanel;
+        public Button createButton;
+        public GameObject checklistItemPrefab;
+
+        string filePath;
+
+        private List<ChecklistObject> checklistObjects = new List<ChecklistObject>();
+
+        private InputField[] addInputFields;
+
+        public class ChecklistItem
+        {
+            public string objName;
+            public int index;
+
+            public ChecklistItem(string name, int index)
+            {
+                this.objName = name;
+                this.index = index;
+            }
+
+        }
+
+        private void Start()
+        {
+            filePath = Application.persistentDataPath + "/checklist.txt";
+            LoadJSONData();
+            addInputFields = addPanel.GetComponentsInChildren<InputField>();
+
+            createButton.onClick.AddListener(delegate
+            {
+                CreateCheckListItems(addInputFields[0].text);
+            });
+        }
+
+        void SwitchMode(int mode)
+        {
+            switch (mode)
+            {
+                case 0:
+                    addPanel.SetActive(false);
+                    break;
+                case 1:
+                    addPanel.SetActive(true);
+                    break;
+            }
+        }
+
+        public void CreateCheckListItems(string name, int loadIndex = 0, bool loading = false)
+        {
+            GameObject item = Instantiate(checklistItemPrefab);
+
+            item.transform.SetParent(content);
+            ChecklistObject itemObject = item.GetComponent<ChecklistObject>();
+            int index = loadIndex;
+            if (!loading)
+                index = checklistObjects.Count;
+
+            itemObject.SetObjectInfo(name, index);
+            checklistObjects.Add(itemObject);
+            ChecklistObject temp = itemObject;
+            itemObject.GetComponent<Toggle>().onValueChanged.AddListener(delegate
+            {
+                CheckItem(temp);
+            });
+            if (!loading)
+            {
+                SaveJSONData();
+                //SwitchMode(0);
+            }
+        }
+
+        void CheckItem(ChecklistObject item)
+        {
+            checklistObjects.Remove(item);
+            SaveJSONData();
+            Destroy(item.gameObject);
+        }
+
+        void SaveJSONData()
+        {
+            string contents = "";
+
+            for (int i = 0; i < checklistObjects.Count; i++)
+            {
+                ChecklistItem temp = new ChecklistItem(checklistObjects[i].objName, checklistObjects[i].index);
+                contents += JsonUtility.ToJson(temp + "\n");
+            }
+            Debug.Log(filePath);
+            //ChecklistObject temp = new ChecklistObject();
+            File.WriteAllText(filePath, contents);
+        }
+
+        void LoadJSONData()
+        {
+            if (File.Exists(filePath))
+            {
+                string contents = File.ReadAllText(filePath);
+                string[] splitContents = contents.Split('\n');
+
+                foreach (string content in splitContents)
+                {
+                    if (content.Trim() != "")
+                    {
+                        Debug.Log(content);
+                        ChecklistItem temp = JsonUtility.FromJson<ChecklistItem>(content);
+                        CreateCheckListItems(temp.objName, temp.index, true);
+                    }
+
+                }
+            }
+            else
+            {
+                Debug.Log("No File!!");
+            }
+
+        }
+    }
+}
--- LTA-Holoapp-Test/Assets/TodoList/Scripts/ChecklistObject.cs
+++ LTA-Holoapp-Test/Assets/TodoList/Scripts/ChecklistObject.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Microsoft.MixedReality.OpenXR.BasicSample
 {
     public class ChecklistObject : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        public string objName;
+        public int index;
+
+        private Text itemText;
+
+        private void Start()
         {
-        
+            itemText = GetComponentInChildren<Text>();
+            itemText.text = objName;
         }
 
-        // Update is called once per frame
-        void Update()
+        public void SetObjectInfo(string name, int index)
         {
-        
+            this.objName = name;
+            this.index = index;
         }
     }
 }
